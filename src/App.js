f42ded0e06b0a2fe17ea8674acadb0cf5800c2d3
jaeyeoneej <5ikve@naver.com>
2024-03-05 18:03:04 +0900

    lazy 적용

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
+import { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./Routes/Home";
-import Test1 from "./Routes/Test1";
-import Test2 from "./Routes/Test2";
-import Test3 from "./Routes/Test3";
+
+const Home = lazy(() => import("./Routes/Home"));
+const Test1 = lazy(() => import("./Routes/Test1"));
+const Test2 = lazy(() => import("./Routes/Test2"));
+const Test3 = lazy(() => import("./Routes/Test3"));
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/test1" element={<Test1 />} />
-        <Route path="/test2" element={<Test2 />} />
-        <Route path="/test3" element={<Test3 />} />
-      </Routes>
+      <Suspense fallback={<div>loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/test1" element={<Test1 />} />
+          <Route path="/test2" element={<Test2 />} />
+          <Route path="/test3" element={<Test3 />} />
+        </Routes>
+      </Suspense>
     </Router>
   );
 }
